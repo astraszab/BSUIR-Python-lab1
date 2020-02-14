200531d2a109fc32799ad23cddf369e89bbb7e54
astraszab <astarshab1@gmail.com>
2020-02-14 09:28:49 +0300

    Add punctuation and uppercase support

--- count_words.py
+++ count_words.py
@@ -1,10 +1,13 @@
 import numpy as np
+import string
 
 
 def count_words(text):
     """Count words in a text.
     Return dictionary of the form {"word": count}
     """
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    text = text.lower()
     words, counts = np.unique(text.split(), return_counts=True)
     return dict(zip(words, counts))
 
